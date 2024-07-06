c9514a7c6041e939d2c91c1f1157aca9e72edbee
Toshan Kirit Jagani <tkjagani@gmail.com>
2024-07-06 18:02:33 +0530

    Rebuilt RNN class

--- model.py
+++ model.py
@@ -8,7 +8,6 @@ data_file = 'data/preprocessed.pkl'
 with open(data_file, 'rb') as file:
     data = pickle.load(file)
 
-
 # Device
 
 device = torch.device(
@@ -18,49 +17,23 @@ device = torch.device(
 )
 
 # Bulding the RNN
-
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.hidden_size = hidden_size
-        self.input_layer = nn.Linear(input_size + hidden_size, hidden_size)
-        self.hidden_layer = nn.Linear(hidden_size, output_size)
+        self.input_layer = nn.Linear(input_size+hidden_size, hidden_size)
+        self.output_layer = nn.Linear(hidden_size, output_size)
         self.relu = nn.ReLU()
-        nn.ModuleList([self.input_layer, self.hidden_layer, self.relu])
+        nn.ModuleList([self.input_layer, self.output_layer, self.relu])
     
-    def forward(self, input, hidden):
-        input_tensor = torch.cat([input, hidden], 1)
+    def forward(self, x, hidden):
+        input_tensor = torch.cat([x, hidden], 1)
 
-        h = self.input_layer(input_tensor)
-        out = self.hidden_layer(h)
-        return((out, h))
+        o = self.input_layer(input_tensor)
+        h = self.relu(o)
+        output = self.output_layer(h)
+
+        return((output, h))
     
     def hidden_init(self):
         return(torch.zeros(1, self.hidden_size))
-
-input_size = 57
-hidden_size = 128
-output_size = data[0][1].size(1)
-
-model = RNN(input_size, hidden_size, output_size).to(device)
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), 0.001)
-
-hidden = model.hidden_init()
-
-def train(model, optimizer, loss_fn):
-    model.train()
-    loss_add = 0
-    for item in data:
-        X = item[0].to(device)
-        Y = item[1].to(device)
-        for inp in X:
-            output, hidden = model(inp, hidden)   
-        loss = loss_fn(output, Y)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        loss_add += loss.item()
-    avg_loss = loss_add/len(data)
-    return(avg_loss)
